09bd40c3c9e0e1ae1f3a81032f90b4d40b56409e
Joabesv <joabevarjao123@gmail.com>
2021-08-26 20:22:49 -0300

    finished lol, css will be added later

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -5,8 +5,8 @@ const addGame = () => {
 
   const game = {
     title: titleInput.value,
-    price: priceInput.value,
-    year: yearInput.value,
+    price: parseFloat(priceInput.value),
+    year: Number(yearInput.value),
   };
 
   // adicionando dados para a API
@@ -21,6 +21,52 @@ const addGame = () => {
     .catch(err => console.log(err));
 };
 
+function deleteGame(listItem) {
+  const id = listItem.getAttribute('data-id');
+  axios
+    .delete(`http://localhost:4444/game/${id}`)
+    .then(() => {
+      alert('Jogo Deletado com sucesso');
+    })
+    .catch(err => console.log(err));
+}
+
+function loadForm(listItem) {
+  const id = listItem.getAttribute('data-id');
+  const title = listItem.getAttribute('data-title');
+  const year = listItem.getAttribute('data-year');
+  const price = listItem.getAttribute('data-price');
+  document.querySelector('#idEdit').value = id;
+  document.querySelector('#titleEdit').value = title;
+  document.querySelector('#yearEdit').value = year;
+  document.querySelector('#priceEdit').value = price;
+}
+
+function updateGame() {
+  const idInput = document.querySelector('#idEdit');
+  const titleInput = document.querySelector('#titleEdit');
+  const priceInput = document.querySelector('#priceEdit');
+  const yearInput = document.querySelector('#yearEdit');
+
+  const game = {
+    title: titleInput.value,
+    price: parseFloat(priceInput.value),
+    year: Number(yearInput.value),
+  };
+
+  const id = idInput.value;
+
+  axios
+    .put(`http://localhost:4444/game/${id}`, game)
+    .then(response => {
+      if (response.status === 200) {
+        alert('Game Atualizado');
+        location.reload();
+      }
+    })
+    .catch(err => console.log(err));
+}
+
 axios
   .get('http://localhost:4444/games')
   .then(response => {
@@ -29,14 +75,36 @@ axios
 
     games.forEach(game => {
       const item = document.createElement('li');
+
+      item.setAttribute('data-id', game.id);
+      item.setAttribute('data-title', game.title);
+      item.setAttribute('data-year', game.year);
+      item.setAttribute('data-price', game.price);
+
       item.innerHTML = `
- Id: ${game.id} <br />
-  Nome: ${game.title}
-  <br /> 
-  Ano de lançamento: ${game.year}
-  <br /> 
-  Preço: R$${game.price}
-  `;
+      Id: ${game.id} <br />
+      Nome: ${game.title}
+      <br /> 
+      Ano de lançamento: ${game.year}
+      <br /> 
+      Preço: R$${game.price}
+      <br />
+      `;
+
+      // button dinâmico para delete
+      const deleteBtn = document.createElement('button');
+      deleteBtn.innerHTML = 'Deletar';
+      deleteBtn.addEventListener('click', () => deleteGame(item));
+
+      // button dinâmico para edit
+
+      const editBtn = document.createElement('button');
+      editBtn.innerHTML = 'Editar';
+      editBtn.addEventListener('click', () => loadForm(item));
+
+      item.appendChild(deleteBtn);
+      item.appendChild(editBtn);
+
       list.appendChild(item);
     });
   })
